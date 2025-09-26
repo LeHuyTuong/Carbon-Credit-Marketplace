8a6fae8965b44e1ed661347e2586f5bd106a793a
giakhiem20051710 <giakhiem20051710@gmail.com>
2025-09-26 23:02:36 +0700

    update login and verifyOtp

--- backend/Market_carbon/src/main/java/com/carbonx/marketcarbon/request/RegisterRequest.java
+++ backend/Market_carbon/src/main/java/com/carbonx/marketcarbon/request/RegisterRequest.java
@@ -3,13 +3,22 @@ package com.carbonx.marketcarbon.request;
 import com.carbonx.marketcarbon.domain.USER_ROLE;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class RegisterRequest {
-    @Email
+    @NotBlank(message = "Email không được để trống")
+    @Email(message = "Email không hợp lệ")
     private String email;
-    private String password;   // tên khớp JSON
+
+    @NotBlank(message = "Password không được để trống")
+    private String password;
+
+    @NotBlank(message = "Full name không được để trống")
     private String fullName;
+
+    @NotNull(message = "Role không được để null")
     private USER_ROLE role;
 }
+
