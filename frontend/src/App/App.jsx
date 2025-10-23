185d3553b8c20afbd848d3f6f6bb61db3f77f474
LeHuyTuong <lehuytuong2005@gmail.com>
2025-10-23 08:53:18 +0700

    Add . to App.jsx

--- frontend/src/App/App.jsx
+++ frontend/src/App/App.jsx
@@ -5,49 +5,49 @@ import {
   Navigate,
   Outlet,
 } from "react-router-dom";
-import { useAuth } from "./context/AuthContext.jsx";
-import Navbar from "./components/Navbar/Navbar.jsx";
-import Home from "./pages/Home/Home.jsx";
-import Login from "./pages/Login/Login.jsx";
-import LoginAdmin from "./pages/loginAdmin/Login.jsx";
-import KYCAdmin from "./pages/KYCAdmin/KYC.jsx";
-import Register from "./pages/Register/Register.jsx";
-import ForgotPassword from "./pages/ForgotPassword/ForgotPassword.jsx";
-import ChangePassword from "./pages/ChangePassword/ChangePassword.jsx";
-import ChangePasswordForm from "./pages/ChangePasswordForm/ChangePasswordForm.jsx";
-import Marketplace from "./pages/Dashboard/Company/Marketplace/Marketplace.jsx";
-import OTP from "./pages/OTP/OTP.jsx";
-import Privacy from "./pages/Term&Privacy/Privacy.jsx";
-import TermsOfUse from "./pages/Term&Privacy/TermsOfUse.jsx";
-import ManageVehicle from "./pages/Dashboard/EVOwner/ManageVehicle/ManageVehicle.jsx";
-import Wallet from "./pages/Wallet/Wallet.jsx";
-import Deposit from "./pages/Wallet/Deposit/Deposit.jsx";
-import Withdraw from "./pages/Wallet/Withdraw/Withdraw.jsx";
-import WalletHistory from "./pages/Wallet/WalletHistory/WalletHistory.jsx";
-import KYC from "./pages/KYC/KYC.jsx";
-import Profile from "./pages/Profile/Profile.jsx";
-import Order from "./pages/Dashboard/Company/Order/Order.jsx";
-import PurchaseHistory from "./pages/Dashboard/Company/PurchaseHistory/PurchaseHistory.jsx";
-import PaymentDetail from "./pages/PaymentDetail/PaymentDetail.jsx";
-import KYCCompany from "./pages/Dashboard/Company/KYCCompany/KYCCompany.jsx";
-import ProfileCompany from "./pages/Dashboard/Company/ProfileCompany/ProfileCompany.jsx";
-import RoleRoute from "./components/RoleRoute.jsx";
-import RegisterProject from "./pages/Dashboard/Company/Projects/RegisterProject.jsx";
-import ListProjects from "./pages/Dashboard/Company/Projects/ListProjects.jsx";
-import DetailProject from "./pages/Dashboard/Company/Projects/DetailProject.jsx";
-import ViewRegisteredProject from "./pages/Dashboard/Company/Projects/ViewRegisteredProject.jsx";
-import ListCredits from "./pages/Dashboard/Company/ManageCredits/ListCredits.jsx";
-import UploadReport from "./pages/Dashboard/Company/Report/UploadReport.jsx";
-import ChooseProjectToUpload from "./pages/Dashboard/Company/Report/ChooseProjectToUpload.jsx";
-import DetailReport from "./pages/Dashboard/Company/Report/DetailReport.jsx";
-import CreditDetail from "./pages/Dashboard/Company/ManageCredits/CreditDetail.jsx";
+import { useAuth } from "../context/AuthContext.jsx";
+import Navbar from "../components/Navbar/Navbar.jsx";
+import Home from "../pages/Home/Home.jsx";
+import Login from "../pages/Login/Login.jsx";
+import LoginAdmin from "../pages/loginAdmin/Login.jsx";
+import KYCAdmin from "../pages/KYCAdmin/KYC.jsx";
+import Register from "../pages/Register/Register.jsx";
+import ForgotPassword from "../pages/ForgotPassword/ForgotPassword.jsx";
+import ChangePassword from "../pages/ChangePassword/ChangePassword.jsx";
+import ChangePasswordForm from "../pages/ChangePasswordForm/ChangePasswordForm.jsx";
+import Marketplace from "../pages/Dashboard/Company/Marketplace/Marketplace.jsx";
+import OTP from "../pages/OTP/OTP.jsx";
+import Privacy from "../pages/Term&Privacy/Privacy.jsx";
+import TermsOfUse from "../pages/Term&Privacy/TermsOfUse.jsx";
+import ManageVehicle from "../pages/Dashboard/EVOwner/ManageVehicle/ManageVehicle.jsx";
+import Wallet from "../pages/Wallet/Wallet.jsx";
+import Deposit from "../pages/Wallet/Deposit/Deposit.jsx";
+import Withdraw from "../pages/Wallet/Withdraw/Withdraw.jsx";
+import WalletHistory from "../pages/Wallet/WalletHistory/WalletHistory.jsx";
+import KYC from "../pages/KYC/KYC.jsx";
+import Profile from "../pages/Profile/Profile.jsx";
+import Order from "../pages/Dashboard/Company/Order/Order.jsx";
+import PurchaseHistory from "../pages/Dashboard/Company/PurchaseHistory/PurchaseHistory.jsx";
+import PaymentDetail from "../pages/PaymentDetail/PaymentDetail.jsx";
+import KYCCompany from "../pages/Dashboard/Company/KYCCompany/KYCCompany.jsx";
+import ProfileCompany from "../pages/Dashboard/Company/ProfileCompany/ProfileCompany.jsx";
+import RoleRoute from "../components/RoleRoute.jsx";
+import RegisterProject from "../pages/Dashboard/Company/Projects/RegisterProject.jsx";
+import ListProjects from "../pages/Dashboard/Company/Projects/ListProjects.jsx";
+import DetailProject from "../pages/Dashboard/Company/Projects/DetailProject.jsx";
+import ViewRegisteredProject from "../pages/Dashboard/Company/Projects/ViewRegisteredProject.jsx";
+import ListCredits from "../pages/Dashboard/Company/ManageCredits/ListCredits.jsx";
+import UploadReport from "../pages/Dashboard/Company/Report/UploadReport.jsx";
+import ChooseProjectToUpload from "../pages/Dashboard/Company/Report/ChooseProjectToUpload.jsx";
+import DetailReport from "../pages/Dashboard/Company/Report/DetailReport.jsx";
+import CreditDetail from "../pages/Dashboard/Company/ManageCredits/CreditDetail.jsx";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 //  import AdminApp
-import AdminApp from "./AdminApp.jsx";
+import AdminApp from "../AdminApp.jsx";
 //  import AdminApp
-import CVAApp from "./CVAApp.jsx";
+import CVAApp from "../CVAApp.jsx";
 
 function Layout() {
   return (
