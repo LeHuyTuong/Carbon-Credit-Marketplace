327b9792fb3bc94f06ad74f6ffb4b5e079d2ad61
LeHuyTuong <lehuytuong2005@gmail.com>
2025-10-26 16:21:44 +0700

    Add import

--- backend/Market_carbon/src/main/java/com/carbonx/marketcarbon/service/impl/CreditIssuanceServiceImpl.java
+++ backend/Market_carbon/src/main/java/com/carbonx/marketcarbon/service/impl/CreditIssuanceServiceImpl.java
@@ -14,6 +14,7 @@ import com.carbonx.marketcarbon.service.EmailService;
 import com.carbonx.marketcarbon.service.credit.SerialNumberService;
 import com.carbonx.marketcarbon.service.credit.SerialNumberService.SerialRange;
 import com.carbonx.marketcarbon.service.credit.formula.CreditFormula;
+import com.carbonx.marketcarbon.utils.CodeGenerator;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,6 +24,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
