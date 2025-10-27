fd722fc37776b1e4f16d8f863ae205446571a902
LeHuyTuong <lehuytuong2005@gmail.com>
2025-10-27 16:37:14 +0700

    Update Delete Payment Details

--- backend/Market_carbon/src/main/java/com/carbonx/marketcarbon/controller/PaymentDetailsController.java
+++ backend/Market_carbon/src/main/java/com/carbonx/marketcarbon/controller/PaymentDetailsController.java
@@ -3,7 +3,7 @@ package com.carbonx.marketcarbon.controller;
 import com.carbonx.marketcarbon.common.StatusCode;
 import com.carbonx.marketcarbon.dto.request.PaymentDetailsRequest;
 import com.carbonx.marketcarbon.model.PaymentDetails;
-import com.carbonx.marketcarbon.service.impl.PaymentDetailsService;
+import com.carbonx.marketcarbon.service.impl.PaymentDetailsServiceImpl;
 import com.carbonx.marketcarbon.utils.Tuong.TuongCommonRequest;
 import com.carbonx.marketcarbon.utils.Tuong.TuongCommonResponse;
 import com.carbonx.marketcarbon.utils.Tuong.TuongResponseStatus;
@@ -21,9 +21,9 @@ import java.util.UUID;
 @RequestMapping("/api/v1/paymentDetails")
 @RequiredArgsConstructor
 public class PaymentDetailsController {
-    private final PaymentDetailsService paymentDetailsService;
+    private final PaymentDetailsServiceImpl paymentDetailsService;
 
-    @Operation(summary = "Create a method to withdrawl" , description = "Add bankAccount to withdrawl")
+    @Operation(summary = "Create a method to withdrawal" , description = "Add bankAccount to withdrawal")
     @PostMapping
     public ResponseEntity<TuongCommonResponse<PaymentDetails>> createPaymentDetails(
             @Valid  @RequestBody TuongCommonRequest<PaymentDetailsRequest> req,
@@ -36,6 +36,42 @@ public class PaymentDetailsController {
         TuongResponseStatus rs = new TuongResponseStatus(StatusCode.SUCCESS.getCode(),
                 StatusCode.SUCCESS.getMessage());
         TuongCommonResponse<PaymentDetails> res = new TuongCommonResponse<>(trace, now , rs , paymentDetails);
+        return ResponseEntity.ok(res);
+    }
+
+    @Operation(summary = "Update method to withdrawal" , description = "Update bankAccount to withdrawal")
+    @PutMapping
+    public ResponseEntity<TuongCommonResponse<PaymentDetails>> updatePaymentDetails(
+            @Valid  @RequestBody TuongCommonRequest<PaymentDetailsRequest> req,
+            @RequestHeader(value = "X-Request-Trace", required = false) String requestTrace,
+            @RequestHeader(value = "X-Request-DateTime", required = false) String requestDateTime
+    ) {
+        String trace = requestTrace != null ? requestTrace : UUID.randomUUID().toString();
+        String now = requestDateTime != null ? requestDateTime : OffsetDateTime.now(ZoneOffset.UTC).toString();
+
+        PaymentDetails paymentDetails = paymentDetailsService.updatePaymentDetails(req.getData());
+
+        TuongResponseStatus rs = new TuongResponseStatus(StatusCode.SUCCESS.getCode(),
+                StatusCode.SUCCESS.getMessage());
+        TuongCommonResponse<PaymentDetails> res = new TuongCommonResponse<>(trace, now , rs , paymentDetails);
+        return ResponseEntity.ok(res);
+    }
+
+    @Operation(summary = "Delete a method to withdrawal" , description = "Delete bankAccount to withdrawal")
+    @DeleteMapping
+    public ResponseEntity<TuongCommonResponse<PaymentDetails>> deletePaymentDetails(
+            @Valid  @RequestBody TuongCommonRequest<PaymentDetailsRequest> req,
+            @RequestHeader(value = "X-Request-Trace", required = false) String requestTrace,
+            @RequestHeader(value = "X-Request-DateTime", required = false) String requestDateTime
+    ) {
+        String trace = requestTrace != null ? requestTrace : UUID.randomUUID().toString();
+        String now = requestDateTime != null ? requestDateTime : OffsetDateTime.now(ZoneOffset.UTC).toString();
+
+        paymentDetailsService.deletePaymentDetails(req.getData());
+
+        TuongResponseStatus rs = new TuongResponseStatus(StatusCode.SUCCESS.getCode(),
+                StatusCode.SUCCESS.getMessage());
+        TuongCommonResponse<PaymentDetails> res = new TuongCommonResponse<>(trace, now , rs , null);
         return ResponseEntity.ok(res);
     }
 
--- backend/Market_carbon/src/main/java/com/carbonx/marketcarbon/service/PaymentDetailsService.java
+++ backend/Market_carbon/src/main/java/com/carbonx/marketcarbon/service/PaymentDetailsService.java
@@ -5,5 +5,10 @@ import com.carbonx.marketcarbon.model.PaymentDetails;
 
 public interface PaymentDetailsService {
     PaymentDetails addPaymentDetails(PaymentDetailsRequest request);
+
+    PaymentDetails updatePaymentDetails(PaymentDetailsRequest request);
+
+    void deletePaymentDetails(PaymentDetailsRequest request);
+
     PaymentDetails getUserPaymentDetails();
 }
