5b739b842e1a4fe7d5d0391acf5abffde4edc710
LeHuyTuong <lehuytuong2005@gmail.com>
2025-11-01 22:03:10 +0700

    Add batch code

--- backend/Market_carbon/src/main/java/com/carbonx/marketcarbon/config/RateLimitConfig.java
+++ backend/Market_carbon/src/main/java/com/carbonx/marketcarbon/config/RateLimitConfig.java
@@ -3,17 +3,35 @@ package com.carbonx.marketcarbon.config;
 import io.github.bucket4j.Bandwidth;
 import io.github.bucket4j.Bucket;
 import io.github.bucket4j.Refill;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Configuration
 public class RateLimitConfig {
-    @Bean
-    public Bucket createBucket() {
-        // Cấu hình: 10 request mỗi phút
-        Bandwidth limit = Bandwidth.classic(10, Refill.greedy(10, Duration.ofMinutes(1)));
-        return Bucket.builder().addLimit(limit).build();
+    @Value("${rate-limit.capacity}")
+    private int capacity;
+
+    @Value("${rate-limit.refill}")
+    private int refill;
+
+    @Value("${rate-limit.duration-minutes}")
+    private int durationInMinutes;
+
+    private final Map<String, Bucket> cache = new ConcurrentHashMap<>(); // bộ nhớ local của instance Spring Boot.
+
+    // Phương thức này để tìm hoặc tạo mới bucket cho 1 IP (apiKey)
+    public Bucket resolveBucket(String apiKey) {
+        return cache.computeIfAbsent(apiKey, this::newBucket);
+    }
+
+    private Bucket newBucket(String apiKey) {
+        Bandwidth limit = Bandwidth.classic(capacity, Refill.greedy(refill, Duration.ofMinutes(durationInMinutes)));
+        return Bucket.builder()
+                .addLimit(limit)
+                .build();
     }
 }
