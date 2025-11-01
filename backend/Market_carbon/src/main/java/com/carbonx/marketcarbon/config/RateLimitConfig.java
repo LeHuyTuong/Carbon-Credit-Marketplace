d414555fbd601583411736d082221591cc8b9c97
LeHuyTuong <lehuytuong2005@gmail.com>
2025-11-01 21:31:48 +0700

    Rate Limit

---
+++ backend/Market_carbon/src/main/java/com/carbonx/marketcarbon/config/RateLimitConfig.java
@@ -0,0 +1,19 @@
+package com.carbonx.marketcarbon.config;
+
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
+public class RateLimitConfig {
+    @Bean
+    public Bucket createBucket() {
+        // Cấu hình: 10 request mỗi phút
+        Bandwidth limit = Bandwidth.classic(10, Refill.greedy(10, Duration.ofMinutes(1)));
+        return Bucket.builder().addLimit(limit).build();
+    }
+}
