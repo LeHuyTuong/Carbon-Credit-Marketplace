52b8672aaecf2433261c35bc739acce1865c957b
LeHuyTuong <lehuytuong2005@gmail.com>
2025-11-04 20:28:32 +0700

    Get retire batch

--- backend/Market_carbon/src/main/java/com/carbonx/marketcarbon/repository/CreditBatchRepository.java
+++ backend/Market_carbon/src/main/java/com/carbonx/marketcarbon/repository/CreditBatchRepository.java
@@ -15,4 +15,5 @@ public interface CreditBatchRepository extends JpaRepository<CreditBatch, Long>,
     Optional<CreditBatch> findWithAllById(Long id);
 
     Optional<CreditBatch> findByIdAndCompanyId(Long id, Long companyId);
+    Optional<CreditBatch> findByCompanyId(Long projectId);
 }
