a35c4fd3e8011c6f55270da6c23d471203680333
LeHuyTuong <lehuytuong2005@gmail.com>
2025-11-12 21:55:23 +0700

    Fix Payout , Sharing logic

--- backend/Market_carbon/src/main/java/com/carbonx/marketcarbon/repository/WalletTransactionRepository.java
+++ backend/Market_carbon/src/main/java/com/carbonx/marketcarbon/repository/WalletTransactionRepository.java
@@ -2,14 +2,16 @@ package com.carbonx.marketcarbon.repository;
 
 import com.carbonx.marketcarbon.common.WalletTransactionType;
 import com.carbonx.marketcarbon.model.*;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface WalletTransactionRepository extends JpaRepository<WalletTransaction, Long> {
     // list theo danh sách order gần nhất
-    List<WalletTransaction> findByWalletOrderByCreatedAtDesc(Wallet wallet);
-
+    @Query("SELECT wt FROM WalletTransaction wt LEFT JOIN FETCH wt.distribution WHERE wt.wallet = :wallet ORDER BY wt.createdAt DESC")
+    List<WalletTransaction> findByWalletOrderByCreatedAtDesc(@Param("wallet") Wallet wallet);
     long count(); // Đếm tổng số giao dịch
 
     long countByWalletUserId(Long userId); // Đếm số giao dịch của một người dùng (thay vì countMyTransactions)
